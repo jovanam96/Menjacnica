dfb585aa76ba558eef7e8379ad4017f40eb5553c
jovanam96 <mitrovicjovana.fon@gmail.com>
2017-03-25 18:41:41 -0700

    Uvedena nefunkcionalna klasa Menjacnica koja implementira interfejs

---
+++ Menjacnica/src/specifikacija/Menjacnica.java
@@ -0,0 +1,27 @@
+package specifikacija;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.Kurs;
+
+public class Menjacnica implements MenjacnicaInterfejs{
+
+	@Override
+	public void dodajKurs(String naziv, Kurs k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKurs(String naziv, Kurs k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs vratiKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
