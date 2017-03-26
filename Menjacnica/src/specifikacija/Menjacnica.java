dce5175dc91a85aa5e34ba5c320c621fd6be343c
jovanam96 <mitrovicjovana.fon@gmail.com>
2017-03-25 18:58:25 -0700

    Uvedena dodatna logicka kontrola u klasi Menjacnica

--- Menjacnica/src/specifikacija/Menjacnica.java
+++ Menjacnica/src/specifikacija/Menjacnica.java
@@ -11,30 +11,36 @@ public class Menjacnica implements MenjacnicaInterfejs{
 
 	@Override
 	public void dodajKurs(String naziv, Kurs k) {
-		for (int i = 0; i < valute.size(); i++) {
-			if(valute.get(i).getNaziv().equals(naziv)) {
-				valute.get(i).getKursevi().add(k);
+		if(k!=null){
+			for (int i = 0; i < valute.size(); i++) {
+				if(valute.get(i).getNaziv().equals(naziv)) {
+					valute.get(i).getKursevi().add(k);
+				}
 			}
 		}
 	}
 
 	@Override
 	public void obrisiKurs(String naziv, Kurs k) { 
-		for (int i = 0; i < valute.size(); i++) {
-			if(valute.get(i).getNaziv().equals(naziv)) {
-				valute.get(i).getKursevi().remove(k);
+		if(k!=null) {
+			for (int i = 0; i < valute.size(); i++) {
+				if(valute.get(i).getNaziv().equals(naziv)) {
+					valute.get(i).getKursevi().remove(k);
+				}
 			}
 		}
 	}
 
 	@Override
 	public Kurs vratiKurs(String naziv, GregorianCalendar datum) {
-		for (int i = 0; i < valute.size(); i++) {
-			if(valute.get(i).getNaziv().equals(naziv)){
-				LinkedList<Kurs> kursevi = valute.get(i).getKursevi();
-				for (int j = 0; j < kursevi.size(); j++) {
-					if(kursevi.get(j).getDatum().equals(datum)) {
-						return kursevi.get(j);
+		if(naziv!=null && datum!=null) {
+			for (int i = 0; i < valute.size(); i++) {
+				if(valute.get(i).getNaziv().equals(naziv)){
+					LinkedList<Kurs> kursevi = valute.get(i).getKursevi();
+					for (int j = 0; j < kursevi.size(); j++) {
+						if(kursevi.get(j).getDatum().equals(datum)) {
+							return kursevi.get(j);
+						}
 					}
 				}
 			}
